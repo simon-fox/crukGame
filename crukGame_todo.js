75fa56efb87521981102274796a3ca353e82f1ae
simon-fox <simon@playlablondon.co.uk>
2013-03-02 14:51:37 +0000

    beginning to set up benchmark test
    with 10k+ entities

---
+++ crukGame_todo.js
@@ -0,0 +1,10 @@
+///////////////////////////////////
+//** CRUK GAMES JAM TO DO LIST **//
+///////////////////////////////////
+
+- testing Impact
+    - generate 10,000 entities in an impact level
+    - across 20,000 pixels?
+    - so have a class pointEntity
+    - then on loading the level, loop 10k times and create them on screen 2-10 pixels apart?
+    
